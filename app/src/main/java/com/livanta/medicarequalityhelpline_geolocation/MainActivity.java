b8d7806ba1d58e3ac04c09bbb51f80c9f64e0067
Rayna <raynaqian@live.com>
2018-01-15 10:18:46 -0500

    Oregon button phone number fixed and added asterisk to Hawaii button. Redundancies removed in button casting.

--- app/src/main/java/com/livanta/medicarequalityhelpline_geolocation/MainActivity.java
+++ app/src/main/java/com/livanta/medicarequalityhelpline_geolocation/MainActivity.java
@@ -23,64 +23,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(new Intent(MainActivity.this, PhoneActivity.class));
         } else {
             setContentView(R.layout.activity_main);
-            Button btn_ak = (Button) findViewById(R.id.button_ak);
+            Button btn_ak = findViewById(R.id.button_ak);
             btn_ak.setOnClickListener(this);
 
-            Button btn_az = (Button) findViewById(R.id.button_az);
+            Button btn_az = findViewById(R.id.button_az);
             btn_az.setOnClickListener(this);
 
-            Button btn_ca = (Button) findViewById(R.id.button_ca);
+            Button btn_ca = findViewById(R.id.button_ca);
             btn_ca.setOnClickListener(this);
 
-            Button btn_ct = (Button) findViewById(R.id.button_ct);
+            Button btn_ct = findViewById(R.id.button_ct);
             btn_ct.setOnClickListener(this);
 
-            Button btn_hi = (Button) findViewById(R.id.button_hi);
+            Button btn_hi = findViewById(R.id.button_hi);
             btn_hi.setOnClickListener(this);
 
-            Button btn_id = (Button) findViewById(R.id.button_id);
+            Button btn_id = findViewById(R.id.button_id);
             btn_id.setOnClickListener(this);
 
-            Button btn_ma = (Button) findViewById(R.id.button_ma);
+            Button btn_ma = findViewById(R.id.button_ma);
             btn_ma.setOnClickListener(this);
 
-            Button btn_me = (Button) findViewById(R.id.button_me);
+            Button btn_me = findViewById(R.id.button_me);
             btn_me.setOnClickListener(this);
 
-            Button btn_nh = (Button) findViewById(R.id.button_nh);
+            Button btn_nh = findViewById(R.id.button_nh);
             btn_nh.setOnClickListener(this);
 
-            Button btn_nj = (Button) findViewById(R.id.button_nj);
+            Button btn_nj = findViewById(R.id.button_nj);
             btn_nj.setOnClickListener(this);
 
-            Button btn_nv = (Button) findViewById(R.id.button_nv);
+            Button btn_nv = findViewById(R.id.button_nv);
             btn_nv.setOnClickListener(this);
 
-            Button btn_ny = (Button) findViewById(R.id.button_ny);
+            Button btn_ny = findViewById(R.id.button_ny);
             btn_ny.setOnClickListener(this);
 
-            Button btn_or = (Button) findViewById(R.id.button_or);
+            Button btn_or = findViewById(R.id.button_or);
             btn_or.setOnClickListener(this);
 
-            Button btn_pa = (Button) findViewById(R.id.button_pa);
+            Button btn_pa = findViewById(R.id.button_pa);
             btn_pa.setOnClickListener(this);
 
-            Button btn_pr = (Button) findViewById(R.id.button_pr);
+            Button btn_pr = findViewById(R.id.button_pr);
             btn_pr.setOnClickListener(this);
 
-            Button btn_ri = (Button) findViewById(R.id.button_ri);
+            Button btn_ri = findViewById(R.id.button_ri);
             btn_ri.setOnClickListener(this);
 
-            Button btn_vi = (Button) findViewById(R.id.button_vi);
+            Button btn_vi = findViewById(R.id.button_vi);
             btn_vi.setOnClickListener(this);
 
-            Button btn_vt = (Button) findViewById(R.id.button_vt);
+            Button btn_vt = findViewById(R.id.button_vt);
             btn_vt.setOnClickListener(this);
 
-            Button btn_wa = (Button) findViewById(R.id.button_wa);
+            Button btn_wa = findViewById(R.id.button_wa);
             btn_wa.setOnClickListener(this);
 
-            Button btn_none = (Button) findViewById(R.id.button_none);
+            Button btn_none = findViewById(R.id.button_none);
             btn_none.setOnClickListener(this);
         }
     }
@@ -95,7 +95,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button_hi:
             case R.id.button_id:
             case R.id.button_nv:
-            case R.id.button_or:
             case R.id.button_wa:
                 // handle phone number change here AREA 5
                 // 1-866-316-6977
@@ -109,6 +108,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button_nh:
             case R.id.button_nj:
             case R.id.button_ny:
+            case R.id.button_or:
             case R.id.button_pa:
             case R.id.button_pr:
             case R.id.button_ri:
